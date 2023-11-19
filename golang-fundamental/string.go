fa5d456063ad14aa0b404b4a729af74c828efb7a
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-19 21:29:43 +0700

    feat: add data type string

---
+++ golang-fundamental/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Auliya")
+	fmt.Println("Auliya Mirzaq")
+	fmt.Println("len(mirzaq) =", len("Mirzaq"))
+	fmt.Println("string[0] =", "Mirzaq"[0])
+}
