881391ff6173e3bb0af8fcf6a9f1882ac157edc6
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-19 22:01:12 +0700

    feat: add variable

---
+++ golang-fundamental/variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "M. Auliya Mirzaq Romdloni"
+	fmt.Println("Nama =", name)
+
+	name = "Auliya Mirzaq"
+	fmt.Println(name)
+
+	var (
+		firstname = "M. Auliya"
+		lastname  = "Mirzaq Romdloni"
+	)
+
+	fmt.Println(firstname, lastname)
+}
