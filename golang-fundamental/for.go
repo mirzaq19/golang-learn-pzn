b36e4bd3377bbf3eecda77ca0fac3ac3f6fc27b0
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-21 13:00:37 +0700

    feat: add for loop

---
+++ golang-fundamental/for.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for counter := 1; counter <= 10; counter++ {
+		fmt.Println("Perulangan ke-", counter)
+	}
+
+	fmt.Println("Selesai")
+
+	names := []string{"Muhammad", "Auliya", "Mirzaq", "Romdloni"}
+	//for i := 0; i < len(names); i++ {
+	//	fmt.Println(names[i])
+	//}
+	for index, name := range names {
+		fmt.Println("Index", index, "=", name)
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
