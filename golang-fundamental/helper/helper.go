56e0191381402c2077ecfbf1e6c247eb66194b9c
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-22 16:17:16 +0700

    feat: add access modifier

--- golang-fundamental/helper/helper.go
+++ golang-fundamental/helper/helper.go
@@ -1,5 +1,19 @@
 package helper
 
+import "fmt"
+
+var version = "1.0.0" //Tidak bisa diakses dari luar
+var Application = "golang"
+
+func sayGoodBye(name string) string {
+	return "Good bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+func Contoh() {
+	sayGoodBye("Mirzaq")
+	fmt.Println(version)
 }
--- golang-fundamental/import.go
+++ golang-fundamental/import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Mirzaq")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	//fmt.Println(helper.version)           // Tidak bisa diakses
+	//fmt.Println(helper.sayGoodBye("Eko")) // Tidak bisa diakses
 }
