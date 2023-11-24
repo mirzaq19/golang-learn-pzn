bf46e1bfd3261765e866c671cb7d5745e7303e06
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-24 13:04:46 +0700

    feat: add fmt

---
+++ golang-standard-library/fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+	firstName := "M. Auliya"
+	lastName := "Mirzaq Romdloni"
+
+	fmt.Println("Hello '", firstName, lastName, "'")
+	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+}
