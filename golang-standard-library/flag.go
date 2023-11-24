f73a6c68140030f3a1e7b1e47726c25bf5e60ca9
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-24 13:29:33 +0700

    feat: add flag

---
+++ golang-standard-library/flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var username *string = flag.String("username", "root", "database username")
+	var password *string = flag.String("password", "1234", "database password")
+	var host *string = flag.String("host", "localhost", "database host")
+	var port *int = flag.Int("port", 0, "database port")
+
+	flag.Parse()
+	fmt.Println("Username", *username)
+	fmt.Println("Password", *password)
+	fmt.Println("Host", *host)
+	fmt.Println("Port", *port)
+}
