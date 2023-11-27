122cee139811a8a8174bc64a20a726036285848e
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-27 18:57:41 +0700

    feat: add app sayhello

---
+++ app-sayhello/go.mod
@@ -0,0 +1,5 @@
+module app-sayhello
+
+go 1.21.3
+
+require github.com/mirzaq19/go-sayhello-module v1.0.2 // indirect
---
+++ app-sayhello/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	gosayhello "github.com/mirzaq19/go-sayhello-module"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
