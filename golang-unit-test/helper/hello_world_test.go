d037582db3f417d5b758f612758016d36a0bcb0b
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-11-29 18:34:08 +0700

    feat: add unit test assertions

--- golang-unit-test/helper/hello_world_test.go
+++ golang-unit-test/helper/hello_world_test.go
@@ -2,8 +2,24 @@ package helper
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Mirzaq")
+	assert.Equal(t, "Hello Mirzaq", result, "Result must be 'Hello Mirzaq'")
+
+	fmt.Println("TestHelloWorld with assert done!")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Mirzaq")
+	require.Equal(t, "Hello Mirzaq", result, "Result must be 'Hello Mirzaq'")
+
+	fmt.Println("TestHelloWorld with require done!")
+}
 
 func TestHelloWorldMirzaq(t *testing.T) {
 	result := HelloWorld("Mirzaq")
