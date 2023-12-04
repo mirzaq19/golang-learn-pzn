6038c1ab2bdd33089317dde4ea21908a72cbe125
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-05 05:43:07 +0700

    feat: add goroutine range channel

--- golang-goroutine/channel_test.go
+++ golang-goroutine/channel_test.go
@@ -2,6 +2,7 @@ package golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -71,6 +72,24 @@ func TestBufferedChannel(t *testing.T) {
 		fmt.Println(<-channel)
 		fmt.Println(<-channel)
 	}()
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke-" + strconv.Itoa(i)
+		}
+
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data:", data)
+	}
 
 	fmt.Println("Selesai")
 }
