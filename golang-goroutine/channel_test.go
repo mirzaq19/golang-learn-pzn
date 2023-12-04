9b1f68d3646878f1c819ebe93d5e71f0fe793b6a
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-04 22:18:04 +0700

    feat: add goroutine channel

---
+++ golang-goroutine/channel_test.go
@@ -0,0 +1,22 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "M. Auliya Mirzaq Romdloni"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
