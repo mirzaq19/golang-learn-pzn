e37c12ae5da76c01fd301ee956dafa49c3b2347b
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-27 15:19:25 +0700

    feat: add sync.pool

---
+++ golang-goroutine/pool_test.go
@@ -0,0 +1,33 @@
+package golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "new"
+		},
+	}
+
+	pool.Put("M.")
+	pool.Put("Auliya")
+	pool.Put("Mirzaq")
+	pool.Put("Romdloni")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			time.Sleep(1 * time.Second)
+			pool.Put(data)
+		}()
+	}
+
+	time.Sleep(11 * time.Second)
+	fmt.Println("Selesai")
+}
