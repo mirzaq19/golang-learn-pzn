2b6dc0e405ad1772b87ad887fcbb0426db27e7d8
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-27 15:04:36 +0700

    feat: add sync.Once

--- golang-goroutine/waitGroup_test.go
+++ golang-goroutine/waitGroup_test.go
@@ -25,3 +25,25 @@ func TestWaitGroup(t *testing.T) {
 	group.Wait()
 	fmt.Println("Complete")
 }
+
+var counter = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	group := sync.WaitGroup{}
+	once := sync.Once{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			group.Add(1)
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	fmt.Println(counter)
+}
