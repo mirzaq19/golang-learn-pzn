e49e3aee44a64890b619a23f70f0ce50b91689c9
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-28 15:12:11 +0700

    feat: context with cancel

--- golang-context/context_test.go
+++ golang-context/context_test.go
@@ -3,7 +3,9 @@ package golang_context
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -38,4 +40,68 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
 	fmt.Println(contextA.Value("b"))
+}
+
+func CreateCounterLeak() chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancelLeak(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	destination := CreateCounterLeak()
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+	return destination
+}
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
 }
