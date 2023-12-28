537affa413f93a55d34f563ee2be5a879ae52be0
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-28 17:10:01 +0700

    feat: query sql

--- golang-database/sql_test.go
+++ golang-database/sql_test.go
@@ -19,3 +19,29 @@ func TestSQLCommand(t *testing.T) {
 
 	fmt.Println("Success insert data to database")
 }
+
+func TestQuerySQL(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	sqlQuery := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, sqlQuery)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
+
+	fmt.Println("Success query data from database")
+}
