caac661708ef9b306ad471845910ebff9cc7b4d1
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-28 16:17:19 +0700

    feat: add function to create connection

---
+++ golang-database/database.go
@@ -0,0 +1,20 @@
+package golang_database
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/golangdb")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(50)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
