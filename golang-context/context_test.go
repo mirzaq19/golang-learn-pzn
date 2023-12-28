6937c014d7fd24da4bdc6aa415d982e99763c634
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2023-12-28 15:22:54 +0700

    feat: context with timeout

--- golang-context/context_test.go
+++ golang-context/context_test.go
@@ -105,3 +105,40 @@ func TestContextWithCancel(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
 }
+func CreateCounterSlow(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	return destination
+}
+func TestContextWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounterSlow(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+}
