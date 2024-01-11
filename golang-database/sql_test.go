febf3866d013dfedcd298e59e537b1cdc9084c3d
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-12 06:14:56 +0700

    feat: database transaction

--- golang-database/sql_test.go
+++ golang-database/sql_test.go
@@ -220,3 +220,37 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment id", id)
 	}
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	// do transaction
+	sqlQuery := "INSERT INTO comments(email,comment) VALUES (?,?)"
+	for i := 0; i < 10; i++ {
+		email := "eko" + strconv.Itoa(i) + "@gmail.com"
+		comment := "komentar ke-" + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, sqlQuery, email, comment)
+		if err != nil {
+			panic(err)
+		}
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment id", id)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+}
