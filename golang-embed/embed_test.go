f6c1b5ce0a5a97bbc8b48d609192cb0b2cb8f8b4
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-12 08:21:30 +0700

    feat: golang embed to byte slice

--- golang-embed/embed_test.go
+++ golang-embed/embed_test.go
@@ -3,6 +3,7 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -11,4 +12,21 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	//err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm) //deprecated
+	file, err := os.OpenFile("logo_new.png", os.O_CREATE|os.O_WRONLY, 0666)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	_, err = file.Write(logo)
+	if err != nil {
+		return
+	}
+
 }
