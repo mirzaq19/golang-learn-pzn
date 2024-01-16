bad61fe8a098526b188a4071df3d4d00749d8c21
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-16 11:13:15 +0700

    feat: create server handler with servemux

--- golang-web/handler_test.go
+++ golang-web/handler_test.go
@@ -21,3 +21,33 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello World")
+	})
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hi")
+	})
+	mux.HandleFunc("/mirzaq", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello Mirzaq")
+	})
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "image")
+	})
+	mux.HandleFunc("/images/thumbnails/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
