8e737853f3cddf3b981912dfe5410eae4198823a
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2024-01-16 18:36:33 +0700

    feat: http test query multiple parameter

--- golang-web/query_param_test.go
+++ golang-web/query_param_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -22,6 +23,41 @@ func TestQueryParameter(t *testing.T) {
 	record := httptest.NewRecorder()
 
 	SayHello(record, request)
+
+	response := record.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request) {
+	firstname := request.URL.Query().Get("firstname")
+	lastname := request.URL.Query().Get("lastname")
+	fmt.Fprintf(writer, "Hello, %s %s", firstname, lastname)
+}
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?firstname=M.%20Auliya&lastname=Mirzaq%20Romdloni", nil)
+	record := httptest.NewRecorder()
+
+	MultipleQueryParameter(record, request)
+
+	response := record.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	names := query["name"]
+	fmt.Fprintf(writer, strings.Join(names, ", "))
+}
+
+func TestMultipleParameterValues(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello?name=M.%20Auliya&name=Mirzaq%20Romdloni", nil)
+	record := httptest.NewRecorder()
+
+	MultipleParameterValues(record, request)
 
 	response := record.Result()
 	body, _ := io.ReadAll(response.Body)
